be00124ca080c17f84064b9eb6cf61efe03a6b30
hoodie13 <74173949+hoodie13@users.noreply.github.com>
2021-01-06 18:13:03 +0800

    我的书源非常稳定，在数据库的搜索数据没必要清除。

--- app/src/main/java/io/legado/app/ui/book/changesource/ChangeSourceViewModel.kt
+++ app/src/main/java/io/legado/app/ui/book/changesource/ChangeSourceViewModel.kt
@@ -100,7 +100,7 @@ class ChangeSourceViewModel(application: Application) : BaseViewModel(applicatio
 
     private fun startSearch() {
         execute {
-            App.db.searchBookDao.clear(name, author)
+            //App.db.searchBookDao.clear(name, author)
             bookSourceList.clear()
             if (searchGroup.isBlank()) {
                 bookSourceList.addAll(App.db.bookSourceDao.allEnabled)
