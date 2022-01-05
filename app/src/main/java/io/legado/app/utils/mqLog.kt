698e22fb95f4c6202cd1e8320b3c730af895b726
hoodie13 <74173949+hoodie13@users.noreply.github.com>
2022-01-05 16:22:58 +0800

    Update mqLog.kt

--- app/src/main/java/io/legado/app/utils/mqLog.kt
+++ app/src/main/java/io/legado/app/utils/mqLog.kt
@@ -4,24 +4,8 @@ import android.util.Log
 import io.legado.app.data.appDb
 
 object mqLog {
-    fun d(s: String) {
-        Log.d("hoodie13", s)
-    }
-
-    fun e(s: String) {
-        Log.e("hoodie13", s)
-    }
+    val TAG = "hoodie13"
 
     fun debug() {
-        val s = "🖼"
-        //"\uD83D\uDDBC"\"
-        d("${s.toStringArray().size} ${s.length}")
     }
-
-    private fun logChapter() {
-        val data = appDb.bookChapterDao.observeAll()
-        d("${data.size} ${data}")
-    }
-
-    var addIndex = 0
 }
